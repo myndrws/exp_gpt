baf72bd4dc493727c5c20268dcb30c3001495be6
myndrws <myndrws@gmail.com>
2023-01-20 14:52:52 +0000

    added a function to use openai edit as well as completion api

--- main.py
+++ main.py
@@ -22,9 +22,20 @@ def get_response(prompt: str, temp: int = 0.5, max_tokens: int = 2048):
     )
 
 
+def get_edit_response(prompt: str, instruction: str, temp: int = 0.5, n: int = 20):
+    return openai.Edit.create(
+        engine="text-davinci-edit-001",
+        input=prompt,
+        instruction=instruction,
+        temperature=temp,
+        n=n
+    )
+
+
 def read_in_prompt_return_outputs(input_directory: str,
                                   output_directory: str,
                                   input_txt_file_name: str,
+                                  complete_or_edit: str = "complete",
                                   **kwargs):
     root_name = input_txt_file_name.replace(".txt", "")
     file_containing_output = root_name + "_response.txt"
@@ -34,7 +45,12 @@ def read_in_prompt_return_outputs(input_directory: str,
         prompt = f.read()
         print(prompt)
 
-    model_response = get_response(prompt=prompt, **kwargs)
+    if complete_or_edit == "complete":
+        model_response = get_response(prompt=prompt, **kwargs)
+    elif complete_or_edit == "edit":
+        model_response = get_edit_response(prompt=prompt, **kwargs)
+    else:
+        raise TypeError("complete_or_edit should be 'complete' or 'edit'")
 
     with open(os.path.join(output_directory, file_containing_output), "w") as f:
         f.write(model_response.choices[0].text)
@@ -48,5 +64,14 @@ def read_in_prompt_return_outputs(input_directory: str,
 if __name__ == "__main__":
     mod_response = read_in_prompt_return_outputs(input_directory="prompt_material",
                                                  output_directory="gpt_output",
-                                                 input_txt_file_name="patient_history_creation_prompt_v4.txt",
+                                                 input_txt_file_name="single_patient_history_piecewise_combined_prompt_response_edited.txt",
                                                  temp=0.8)
+
+    edit_response = read_in_prompt_return_outputs(input_directory="prompt_material",
+                                                  output_directory="gpt_output",
+                                                  input_txt_file_name="__.txt",
+                                                  temp=0.8,
+                                                  complete_or_edit="edit",
+                                                  instruction="Add in some mistakes and irrelevant details.",
+                                                  n=20)
+
